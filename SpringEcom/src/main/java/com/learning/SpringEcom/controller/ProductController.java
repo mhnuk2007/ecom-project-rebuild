066f78cfd9979883ab448c1bc471e4de01b50686
Mohan Lal <mhnuk2007@gmail.com>
2025-09-23 00:34:22 +0500

    Implement Product CRUD with image upload, search, and PostgreSQL config

--- SpringEcom/src/main/java/com/learning/SpringEcom/controller/ProductController.java
+++ SpringEcom/src/main/java/com/learning/SpringEcom/controller/ProductController.java
@@ -80,6 +80,14 @@ public class ProductController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+
+    @GetMapping("/products/search")
+    public ResponseEntity<List<Product>> searchProducts(@RequestParam String keyword){
+        List<Product> products = productService.searchProducts(keyword);
+        System.out.println("searching with " + keyword);
+        return new ResponseEntity<>(products, HttpStatus.OK);
+
+    }
 }
 
 
