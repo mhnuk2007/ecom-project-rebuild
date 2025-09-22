6cde27a5530561dec75337b5624f0b76e1beb551
Mohan Lal <mhnuk2007@gmail.com>
2025-09-22 23:48:24 +0500

    Add CRUD operations with image upload/download support for Product entity

--- SpringEcom/src/main/java/com/learning/SpringEcom/controller/ProductController.java
+++ SpringEcom/src/main/java/com/learning/SpringEcom/controller/ProductController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
@@ -32,4 +33,53 @@ public class ProductController {
         }
     }
 
+    @PostMapping("/product")
+    public ResponseEntity<?> addProduct(@RequestPart Product product, @RequestPart MultipartFile imageFile) {
+        try {
+            Product savedProduct = productService.addOrUpdateProduct(product, imageFile);
+            return new ResponseEntity<>(savedProduct, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("/product/{id}/image")
+    public ResponseEntity<byte[]> getProductImage(@PathVariable int id) {
+        Product product = productService.getProductById(id);
+        if (product.getId() > 0) {
+            return new  ResponseEntity<>(product.getImageData(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PutMapping("/product/{id}")
+    public ResponseEntity<?> updateProduct(@PathVariable int id, @RequestPart Product product, @RequestPart MultipartFile imageFile) {
+
+        Product existingProduct = productService.getProductById(id);
+        if (existingProduct.getId() > 0) {
+            try {
+                Product updatedProduct = productService.addOrUpdateProduct(product, imageFile);
+                return new ResponseEntity<>(updatedProduct, HttpStatus.OK);
+            } catch (Exception e) {
+                return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+            }
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/product/{id}")
+    public ResponseEntity<?> deleteProduct(@PathVariable int id) {
+        Product existingProduct = productService.getProductById(id);
+        if (existingProduct.getId() > 0) {
+            productService.deleteProduct(id);
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
+
+
--- SpringEcom/src/main/java/com/learning/SpringEcom/service/ProductService.java
+++ SpringEcom/src/main/java/com/learning/SpringEcom/service/ProductService.java
@@ -4,7 +4,9 @@ import com.learning.SpringEcom.model.Product;
 import com.learning.SpringEcom.repo.ProductRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
@@ -20,4 +22,16 @@ public class ProductService {
     public Product getProductById(int id) {
         return productRepo.findById(id).orElse(new Product(-1));
     }
+
+    public Product addOrUpdateProduct(Product product, MultipartFile imageFile) throws IOException {
+        product.setImageName(imageFile.getOriginalFilename());
+        product.setImageType(imageFile.getContentType());
+        product.setImageData(imageFile.getBytes());
+        return productRepo.save(product);
+    }
+
+    public void deleteProduct(int id) {
+        productRepo.deleteById(id);
+    }
 }
+
